055c59a3f78bd95e1ce97a776128eaed66630477
sarvesh-df <sagrawal@deployflow.co>
2024-12-10 17:14:02 +0530

    fix: problem to read js files

--- innovatios/innovatios/settings.py
+++ innovatios/innovatios/settings.py
@@ -116,13 +116,25 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#   os.path.join(BASE_DIR ,'static')
+# ]
+# STATIC_ROOT = os.path.join(BASE_DIR,'assets')
+
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'assets')
+# ]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static_collected')  # a different directory
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-  os.path.join(BASE_DIR ,'static')
+    os.path.join(BASE_DIR, 'static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR,'assets')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# DEBUG = True
